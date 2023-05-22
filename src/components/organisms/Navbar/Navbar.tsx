49a0f680fbfdcb2f37fb99472e2da8dc276e4122
nathan-nando <nathan.nandoo@gmail.com>
2023-05-22 11:24:00 +0700

    add navbar

---
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { NavLink, useLocation } from "react-router-dom";
+import { Nav, NavItem } from "react-bootstrap";
+import styles from "./navbar.module.css";
+
+export const Navbar = () => {
+  const location = useLocation();
+  return (
+    <div className={styles.navbarWrapper}>
+      <Nav activeKey={location.pathname} defaultActiveKey={"/"}>
+        <NavItem>
+          <NavLink to="/" className="nav-link">
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/dana-pensiun" className="nav-link">
+            Dana pensiun
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  );
+};
--- src/features/Root/Root.tsx
+++ src/features/Root/Root.tsx
@@ -1,33 +1,15 @@
 import styles from "./root.module.css";
-import { NavLink, Outlet, useLocation } from "react-router-dom";
 import { FC } from "react";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../../components/organisms/Navbar/Navbar";
 
 type Props = {};
 
 export const Root: FC = (props: Props) => {
-  const location = useLocation();
   return (
     <div className={styles.root}>
-      <Navbar>
-        <Nav
-          className={styles.headerNavigator}
-          activeKey={location.pathname}
-          defaultActiveKey={"/"}
-        >
-          <NavItem>
-            <NavLink to="/" className="nav-link">
-              Home
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to="/dana-pensiun" className="nav-link">
-              Dana Pensiun
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
+      <Navbar />
       <Outlet />
     </div>
   );
